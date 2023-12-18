606c58cc6c113f8f2f9bd2991d700f465b997e4d
bankkkay <qwertoz322@gmail.com>
2023-12-18 18:50:34 +0700

    change + add file

--- news_project/news_app/apps.py
+++ news_project/news_app/apps.py
@@ -1,5 +1,9 @@
+# Ваш файл apps.py в директории news_app
 from django.apps import AppConfig
 
 class NewsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_app'
+
+    def ready(self):
+        from news_app.signals import delete_news_images
